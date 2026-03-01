2da8a6b15a5bbfd9a19c48111ead564e30aa7899
Tabasco1410 <traine233.yk@gmail.com>
2026-03-01 09:48:12 +0900

    一部Mod名変更に伴うResource名の変更

--- Patches/MainMenuManagerPatch.cs
+++ Patches/MainMenuManagerPatch.cs
@@ -180,7 +180,7 @@ namespace TownOfHostY
                 ModWarningText.transform.localPosition = new Vector3(1.4f, 0.25f, 0f);
                 ModWarningText.transform.localScale = new Vector3(1.9f, 1.9f, 2f);
                 var renderer = ModWarningText.AddComponent<SpriteRenderer>();
-                renderer.sprite = Utils.LoadSprite($"TownOfHost_Y.Resources.warning_online.png", 400f);
+                renderer.sprite = Utils.LoadSprite($"TownOfHost_Y-ForkedbyTabasco.Resources.warning_online.png", 400f);
             }
         }
     }
