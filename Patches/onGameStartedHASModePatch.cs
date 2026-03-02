ad85cae49c64e559b021f58e0629d34091581b9a
Tabasco1410 <traine233.yk@gmail.com>
2026-03-02 23:54:36 +0900

    かくれんぼモードの修正を試みる/メモ
    デバイス変更するのでメモ。

--- Patches/onGameStartedHASModePatch.cs
+++ Patches/onGameStartedHASModePatch.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
 using AmongUs.GameOptions;
 using HarmonyLib;
-
+using Hazel;
 using TownOfHostY.Roles;
 using TownOfHostY.Roles.Core;
 
@@ -11,22 +12,71 @@ class SelectRolesHASModePatch
 {
     public static bool Prefix(RoleManager __instance)
     {
-        if (!AmongUsClient.Instance.AmHost) return false;
+        if (!AmongUsClient.Instance.AmHost) return true;
 
-        //HideAndSeek用
-        if (!Options.IsHASMode) return false;
+        if (!Options.IsHASMode) return true;
 
-        //CustomRpcSenderとRpcSetRoleReplacerの初期化
-        RpcSetRoleReplacer.StartReplace();
+
+        Dictionary<byte, CustomRpcSender> senders = new();
+        foreach (var pc in Main.AllPlayerControls)
+        {
+            senders[pc.PlayerId] = new CustomRpcSender($"{pc.name}'s SetRole Sender", SendOption.None, false)
+                .StartMessage(pc.GetClientId());
+        }
+        RpcSetRoleReplacer.StartReplace(senders);
 
         RoleAssignManager.SelectAssignRoles();
+
+        List<PlayerControl> AllPlayers = new();
+        foreach (var pc in Main.AllPlayerControls)
+            AllPlayers.Add(pc);
 
         //バニラの役職割り当て
         SelectRolesPatch.AssignRolesNormal(null, 0);
 
-        //MODの役職割り当て
-        RpcSetRoleReplacer.Release(); //保存していたSetRoleRpcを一気に書く
+        return false;
+    }
 
+    public static void Postfix()
+    {
+        if (!AmongUsClient.Instance.AmHost) return;
+
+        if (!Options.IsHASMode) return;
+
+        //MODの役職割り当て
+        RpcSetRoleReplacer.Release();
+        RpcSetRoleReplacer.senders.Do(kvp => kvp.Value.SendMessage());
+
+        //リセット処理
+        RpcSetRoleReplacer.senders = null;
+        RpcSetRoleReplacer.OverriddenSenderList = null;
+        RpcSetRoleReplacer.StoragedData = null;
+        SelectRolesPatch.Disconnected.Clear();
+
+        //HAS役職割り当て
+        List<PlayerControl> Crewmates = new();
+        List<PlayerControl> Impostors = new();
+
+        foreach (var pc in Main.AllPlayerControls)
+        {
+            pc.Data.IsDead = false;
+            var state = PlayerState.GetByPlayerId(pc.PlayerId);
+            if (state.GetNowMainRole() != CustomRoles.NotAssigned) continue;
+
+            var role = CustomRoles.NotAssigned;
+            switch (pc.Data.Role.Role)
+            {
+                case RoleTypes.Crewmate: Crewmates.Add(pc); role = CustomRoles.Crewmate; break;
+                case RoleTypes.Impostor: Impostors.Add(pc); role = CustomRoles.Impostor; break;
+                default:
+                    Logger.SendInGame(string.Format(Translator.GetString("Error.InvalidRoleAssignment"), pc?.Data?.PlayerName));
+                    break;
+            }
+            if (role != CustomRoles.NotAssigned)
+                state.SetMainRole(role);
+        }
+
+        //色設定処理
         SetColorPatch.IsAntiGlitchDisabled = true;
         foreach (var pc in Main.AllPlayerControls)
         {
@@ -43,20 +93,18 @@ class SelectRolesHASModePatch
             SelectRolesPatch.AssignCustomRolesFromList(CustomRoles.HASFox, list);
             SelectRolesPatch.AssignCustomRolesFromList(CustomRoles.HASTroll, list);
         }
+
         foreach (var pair in PlayerState.AllPlayerStates)
         {
             //RPCによる同期
             ExtendedPlayerControl.RpcSetCustomRole(pair.Key, pair.Value.GetNowMainRole());
         }
-        //色設定処理
-        SetColorPatch.IsAntiGlitchDisabled = true;
+
+        SetColorPatch.IsAntiGlitchDisabled = false;
 
         GameEndChecker.SetPredicateToHideAndSeek();
 
         Utils.CountAlivePlayers(true);
         Utils.SyncAllSettings();
-        SetColorPatch.IsAntiGlitchDisabled = false;
-
-        return false;
     }
 }
